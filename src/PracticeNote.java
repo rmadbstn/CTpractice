39bd8eb35e50b779f4309cc1aff892a27c45e89b
yskeum <rmadbstn@naver.com>
2022-03-25 19:54:59 +0900

    BackJoon10757 Not Resolved 2022_03_25 19:54

---
+++ src/PracticeNote.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PracticeNote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		
+		for(int i = 0; i<testCase; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+
+	}
+
+}
