7fb27ccda8ee34c7b00d2b690670ca22cfb2424f
yskeum <rmadbstn@naver.com>
2022-04-05 01:14:31 +0900

    2022_04_05 01:14 BackJoon11729 Finished

--- src/RecursiveFunction/BackJoon11729.java
+++ src/RecursiveFunction/BackJoon11729.java
@@ -1,43 +1,41 @@
 package RecursiveFunction;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class BackJoon11729 {
+	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-	Scanner sc = new Scanner(System.in);
+//	Scanner sc = new Scanner(System.in);         //¹öÆÛ ½á¾ß ºü¸£´ÙÀ×.
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//	int n = sc.nextInt();
+	int n = Integer.parseInt(br.readLine());
 	
-	int n = sc.nextInt();
-	
-	
-	System.out.println(hanoi(n));
+	bw.write((int)Math.pow(2, n)-1+"\n"); //t(n) = 2t(n-1)+1 = 2^n -1
 	hanoiWay(n,1,2,3);
-	
+	br.close();
+	bw.flush();
+	bw.close();
 
 	}
 	
-	static int hanoi(int n) {
-		
-		
-		if(n==1) {
-			return 1;
-		}
-		
-		
-		return 2*hanoi(n-1)+1; //È½¼ö
-		
-		
-	}
 	
-	static void hanoiWay(int n,int from,int temp, int to) {
+	static void hanoiWay(int n,int from,int temp, int to) throws IOException{
 		if(n==1) {
-			System.out.println(from+" "+to);
+			bw.write(from+" "+to+"\n");
 		}
 		else {
+			
+			
 			hanoiWay(n-1,from,to,temp);         //temp ->to  to ->temp
-			System.out.println(from+" "+to);
-			hanoiWay(n-1,temp,from,to);			//temp ->from from->temp
+			bw.write(from+" "+to+"\n");
+			hanoiWay(n-1,temp,from,to);			//temp ->from from->temp       
 		}
 	}
 
