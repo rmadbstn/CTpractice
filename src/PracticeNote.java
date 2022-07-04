bb00fed0d37f0b2f987b90510af6120408f6a09f
yskeum <rmadbstn@naver.com>
2022-07-05 00:41:04 +0900

    commit test

--- src/PracticeNote.java
+++ src/PracticeNote.java
@@ -5,24 +5,7 @@ import java.io.InputStreamReader;
 public class PracticeNote {
 
 	public static void main(String[] args)throws IOException{
-       	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-        for(int x=0;x<n;x++){
-            for(int y=0;y<n;y++){
-                star(x,y,n);
-            }
-            System.out.print("\n");
-        }  
+       System.out.println(6%10);
     }
-    public static void star(int x, int y, int n){
-        if((x/n)%3 == 1 && (y/n)%3 == 1){
-            System.out.print(" ");
-        }else{
-            if(n/3==0){                                //x,y»ç 
-                System.out.print("*");
-            }else{
-                star(x,y,n/3);
-            }
-        }
-    }
+   
 }
