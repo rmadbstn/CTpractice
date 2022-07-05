6c0dfac10e5bdb580b33bab965e023de33bb91bc
yskeum <rmadbstn@naver.com>
2022-07-06 00:11:38 +0900

    BaekJoon1436 finished

--- src/PracticeNote.java
+++ src/PracticeNote.java
@@ -5,7 +5,14 @@ import java.io.InputStreamReader;
 public class PracticeNote {
 
 	public static void main(String[] args)throws IOException{
-       System.out.println(6%10);
+		int k = 1044656;
+		
+	   String test = String.valueOf(k);
+	   
+	   if(test.contains("666")) {
+		   System.out.println("포함하지롱");
+	   }
+       System.out.println(test);
     }
    
 }
