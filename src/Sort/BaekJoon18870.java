46b20b7fdae2478cbb3b791d9751d0792df163fd
yskeum <rmadbstn@naver.com>
2022-07-07 23:14:18 +0900

    BaekJoon18870 finished

--- src/Sort/BaekJoon18870.java
+++ src/Sort/BaekJoon18870.java
@@ -1,29 +1,51 @@
 package Sort;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class BaekJoon18870 {
 
 	public static void main(String[] args) throws IOException{
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner sc = new Scanner(System.in);
 		
-		int n = Integer.parseInt(br.readLine());
-		String text = br.readLine();
-		String[] word = text.split(" ");
+		int N = sc.nextInt();
 		
-		int[] arr = new int[n];
-		for(int i =0;i<n;i++) {
-			arr[i]=Integer.parseInt(word[i]);			
+		int[] originArr = new int[N];
+		int[] sortedArr = new int[N];
+		
+		
+		HashMap<Integer,Integer> rankMap = new HashMap<Integer,Integer>();
+		
+		for(int i=0;i<N;i++) {
+			sortedArr[i]=originArr[i] = sc.nextInt();
 		}
 		
+		Arrays.sort(sortedArr);
+		int rank=0;
+		for(int i:sortedArr) {
+			
+			if(!rankMap.containsKey(i)) {
+				rankMap.put(i, rank);  
+				rank++;                    
+			}
+			
+			
+		}
 		
-		System.out.println("text");
+		StringBuilder sb = new StringBuilder();
+		for(int key:originArr) {
+			int resultRank = rankMap.get(key);
+			sb.append(resultRank).append(' ');
+		}
+		
+		System.out.println(sb);
+		
+		
+		
+		
 		
 		
 	}
