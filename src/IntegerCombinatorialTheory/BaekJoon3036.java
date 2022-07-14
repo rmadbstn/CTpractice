fc7156c334471a21abc435b3b65b881c03e5920f
KEUMYUNSU <rmadbstn@naver.com>
2022-07-14 18:56:25 +0900

    BaekJoon2477 finish BaekJoon3036 ing

---
+++ src/IntegerCombinatorialTheory/BaekJoon3036.java
@@ -0,0 +1,40 @@
+package IntegerCombinatorialTheory;
+
+import java.util.Scanner;
+
+public class BaekJoon3036 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		
+		for(int i=1;i<n;i++) {
+			int checkNum=0;
+			while(arr[0]<=checkNum) {
+				
+				
+				checkNum = arr[i];
+				
+			}
+				
+				
+				
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
