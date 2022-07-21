5233ff387cd57114ecb73ad97efa5e78f112ea27
yskeum <rmadbstn@naver.com>
2022-07-21 23:35:43 +0900

    BaekJoon 9663 ing..

--- src/Backtracking/BaekJoon9663.java
+++ src/Backtracking/BaekJoon9663.java
@@ -4,70 +4,55 @@ import java.util.Scanner;
 
 public class BaekJoon9663 {
 
-	public static void main(String[] args) {
+	public static int N;
+	public static int count = 0;
+	public static int[] chessCol;
+
+
+	
+	static boolean isPossible(int level) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		
-		boolean[][] chessBoard = new boolean[N][N];
-		
-		for(int i=0;i<N;i++) {
+		for(int i = 1; i<level; i++) {
 			
-			for(int j =0;j<N;j++) {
-				
-				dfs(chessBoard,i,j,1);
-				
-			}
+			
 			
 		}
 		
 		
-		System.out.println(count);
+	}
+	
+	
+	
+	static boolean setQueen(int level) {
 		
+		if(!isPossible(level)) {
+			
+			return false;
+		}
 		
-	}	
-	
-	static int N;
-	static int count=0;
-	
-	
-	
-	public static void dfs(boolean[][] chessBoard,int x, int y,int queenCount) {
-		
-		if(queenCount==N) {
+		else if(level == N) {
+			
 			count++;
-			return;
-		}	
-		
-		
-		
-		
-		for(int i=0;i<N;i++) {
-			
-			
-			for(int j=0;j<N;j++) {
-				
-				if(x-i==y-j || x-i==-(y-j) || i==x || j == x || i==x && j == y) {
-					chessBoard[i][j]=true;
-				}
-				
-				if(!chessBoard[i][j]) {
-					queenCount++;
-					
-					dfs(chessBoard,i,j,queenCount);
-//					queenCount--;
-				}
-					
-				
-				
-			}
-			
-			
+			return true;
 		}
 		
+		for ( int i = 0;i<N;i++) {
+			chessCol[level] = i;
+			setQueen(level+1);
+		}
 		
+	}
+	
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		chessCol = new int[N];
 		
+		set
 	}
 
 }
