fa80d5bc0e8f00584e7d58bb53f55bddb7489150
KEUMYUNSU <rmadbstn@naver.com>
2022-09-02 14:51:12 +0900

    BaekJoon1753 finish

--- src/ClassQuestion/BaekJoon1753_final.java
+++ src/ClassQuestion/BaekJoon1753_final.java
@@ -5,60 +5,63 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
+class Node implements Comparable<Node> {
 
-class Node implements Comparable<Node>{
-	
-	int end,weight;
-	
-	public Node(int end,int weight) {
-		
+	int end, weight;
+
+	public Node(int end, int weight) {
+
 		this.end = end;
 		this.weight = weight;
 	}
 
 	@Override
 	public int compareTo(Node o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return weight - o.weight;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
 
+public class BaekJoon1753_final {
 
+	static int[] distance;
+	static int INF = 600000001;
+	static int V, E;
 
-public class BaekJoon1753_final{
-	
-	
-	public static void bfs(int K, LinkedList<Integer>[] list) {
+	public static void dijkstra(int K, LinkedList<Node>[] list) {
 
-		Queue<Integer> queue = new LinkedList<>();
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		boolean[] check = new boolean[V + 1];
+		
+		
 
-		queue.offer(K);
+		queue.offer(new Node(K, 0));
 
 		while (!queue.isEmpty()) {
 
-			int cur = queue.poll();
+			Node curNode = queue.poll();
 
-			for (int end : list[cur]) {
-
-				if (dpArr[cur] + wArr[cur][end] >= dpArr[end] && dpArr[end] != 0) {
-					continue;
-				}
-
-				dpArr[end] = dpArr[cur] + wArr[cur][end];
+			int temp = curNode.end;
+			if(check[temp]==true) {
+				continue;
+			}else {
+				check[temp ]= true;
+			}
 			
-				queue.offer(end);
+			
+			for (Node endNode : list[curNode.end]) {
+
+				if (distance[endNode.end] > distance[curNode.end] + endNode.weight) {
+					distance[endNode.end] = distance[curNode.end] + endNode.weight;
+					queue.offer(new Node(endNode.end, distance[endNode.end]));
+//					System.out.println(endNode.end);
+
+				}
 
 			}
 
@@ -71,16 +74,17 @@ public class BaekJoon1753_final{
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String[] strArr = br.readLine().split(" ");
-		int V = Integer.parseInt(strArr[0]); // 정점의 개수
-		int E = Integer.parseInt(strArr[1]); // 간선의 개수
+		V = Integer.parseInt(strArr[0]); // 정점의 개수
+		E = Integer.parseInt(strArr[1]); // 간선의 개수
 
-		wArr = new int[V + 1][V + 1]; // 두 정점간 가중치.
-		dpArr = new int[V + 1];// 최소 가중치 합 저장.
+		distance = new int[V + 1];// 최소 가중치 합 저장.
 
-		LinkedList<Integer>[] list = new LinkedList[V + 1];
+		Arrays.fill(distance, INF);
+
+		LinkedList<Node>[] list = new LinkedList[V + 1];
 
 		for (int i = 1; i <= V; i++) {
-			
+
 			list[i] = new LinkedList<>();
 
 		}
@@ -89,6 +93,7 @@ public class BaekJoon1753_final{
 
 		int K = Integer.parseInt(str);// 시작 점 .
 
+		distance[K] = 0;
 		for (int i = 0; i < E; i++) {
 
 			String[] strArr2 = br.readLine().split(" ");
@@ -96,29 +101,26 @@ public class BaekJoon1753_final{
 			int end = Integer.parseInt(strArr2[1]); // 도착점
 			int w = Integer.parseInt(strArr2[2]); // 가중치
 
-			wArr[start][end] = w;
-			list[start].add(end);
+			Node node = new Node(end, w);
+			list[start].add(node);
 
 		}
 
-		bfs(K, list);
+		dijkstra(K, list);
 
-		for (int i = 1; i < V+1; i++) {
+		for (int i = 1; i < V + 1; i++) {
 
-			if(i==K) {
-				bw.write("0"+"\n");
-			}else if(dpArr[i]==0) {
-				bw.write("INF"+"\n");
-			}else {
-			bw.write(dpArr[i]+"\n");
-			
+			if (i == K) {
+				bw.write("0" + "\n");
+			} else if (distance[i] == INF) {
+				bw.write("INF" + "\n");
+			} else {
+				bw.write(distance[i] + "\n");
+
 			}
 		}
-
 		bw.flush();
 		bw.close();
 	}
 
 }
-
-
