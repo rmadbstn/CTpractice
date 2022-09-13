84332fcf057d807156c579ea59a52ca756a4498f
KEUMYUNSU <rmadbstn@naver.com>
2022-09-13 23:31:05 +0900

    2022 09 13 BaekJoon Study

--- src/PracticeNote.java
+++ src/PracticeNote.java
@@ -1,26 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class PracticeNote {
 
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i=1;i<6;i++) {
-			list.add(i);
-		}
-		list.remove(2);
-		
-		for(int a : list) {
-			System.out.println(a);
-		}
-//		System.out.println(list.get(4));
 
+		int a = 'A';
+		System.out.println(a-65);
+		
 	}
 
 }
