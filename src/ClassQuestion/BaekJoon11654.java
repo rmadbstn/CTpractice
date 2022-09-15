1dfc8cc247609d5c42ea0984020025922d2ce04a
KEUMYUNSU <rmadbstn@naver.com>
2022-09-15 19:48:27 +0900

    BaekJoon11654 finish

---
+++ src/ClassQuestion/BaekJoon11654.java
@@ -0,0 +1,22 @@
+package ClassQuestion;
+
+import java.util.Scanner;
+
+public class BaekJoon11654 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		int a = str.charAt(0);
+		
+		System.out.println(a);
+	
+
+	
+	}
+	
+
+}
