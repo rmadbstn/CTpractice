53afb91840fa919f14b680044d02defcc6551727
KEUMYUNSU <rmadbstn@naver.com>
2022-10-06 11:00:14 +0900

    BaekJoon14891 finish

--- src/SamSung/BaekJoon14891.java
+++ src/SamSung/BaekJoon14891.java
@@ -9,9 +9,11 @@ public class BaekJoon14891 {
 
 	static int[][] arr;
 	static int[] cursors;
+	static boolean[] check;
 
 	public static int[] spinWheel(int[] wheelArr, int dir) {
 
+		
 		if (dir == 1) {
 
 			wheelArr[0] = wheelArr[8];
@@ -38,40 +40,36 @@ public class BaekJoon14891 {
 
 	}
 
-	public static void wheel(int wheelIndex, int prevIndex, int dir) {
+	public static void wheel(int wheelIndex,  int dir) {
 
-		if (wheelIndex > prevIndex) {
+		check[wheelIndex]=true;
+		
+		
+		if(wheelIndex-1>=1&&check[wheelIndex-1]==false) {
 			
-
-			if (arr[prevIndex][3] != arr[wheelIndex][7]) {
-
-				if (wheelIndex + 1 <= 3) {
-
-					wheel(wheelIndex + 1, wheelIndex, dir*(-1));
-				}
-
-				arr[wheelIndex] = spinWheel(arr[wheelIndex], dir*(-1));
-
+			if(arr[wheelIndex][7]!=arr[wheelIndex-1][3]) {
+				wheel(wheelIndex-1,dir*(-1));
 			}
-			
-			
-
-		}else {
-			
-			if (arr[prevIndex][7] != arr[wheelIndex][3]) {
-
-				if (wheelIndex - 1 >= 1) {
-
-					wheel(wheelIndex - 1, wheelIndex, dir*(-1));
-				}
-
-				arr[wheelIndex] = spinWheel(arr[wheelIndex], dir*(-1));
-
-			}
-			
-			
 			
 		}
+		
+		
+		if(wheelIndex+1<=4&&check[wheelIndex+1]==false) {
+			
+			if(arr[wheelIndex][3]!=arr[wheelIndex+1][7]) {
+				wheel(wheelIndex+1,dir*(-1));
+			}
+		}
+		
+		
+		
+//		System.out.print(wheelIndex+"번째:"+Arrays.toString(arr[wheelIndex])+"->");
+		arr[wheelIndex]= spinWheel(arr[wheelIndex],dir);
+		
+//		System.out.print(Arrays.toString(arr[wheelIndex]));
+
+//		System.out.println();
+		
 
 	}
 
@@ -89,7 +87,7 @@ public class BaekJoon14891 {
 
 			for (int j = 1; j <= 8; j++) {
 
-				arr[i][j] = str.charAt(i - 1) - '0';
+				arr[i][j] = str.charAt(j - 1) - '0';
 
 			}
 
@@ -104,35 +102,36 @@ public class BaekJoon14891 {
 			int wheelIndex = Integer.parseInt(strArr[0]);
 			int dir = Integer.parseInt(strArr[1]); // 1:시계방향, -1:반시계방향.
 
-			if (wheelIndex == 2 || wheelIndex == 3) {
-
-				wheel(wheelIndex - 1, wheelIndex, dir);
-				wheel(wheelIndex + 1, wheelIndex, dir);
-
-			} else if (wheelIndex == 1) {
-				wheel(wheelIndex + 1, wheelIndex, dir);
-			} else {
-				wheel(wheelIndex - 1, wheelIndex, dir);
-			}
-
-			arr[wheelIndex] = spinWheel(arr[wheelIndex], dir);
+			check = new boolean[5];
+			
+			wheel(wheelIndex,dir);
 
 		}
 		
 		
-		int result = 0;
+		int result = 0;		
 		
 		
 		for(int i=1;i<=4;i++) {
 			
-			if(arr[i][1]==1) {
-				result+= 2*(i-1);
+//			System.out.println(Arrays.toString(arr[i]));
+//			System.out.println("arr[i][1]:"+arr[i][1]);
+			
+			if(arr[i][1] == 1) {
+				
+			
+				
+				result+=Math.pow(2,i-1);
+				
+//				System.out.println(result);
 			}
+			
 			
 		}
 		
-		System.out.println(result);
 
+		System.out.println(result);
+		
 	}
 
 }
