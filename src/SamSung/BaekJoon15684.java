07ff118f085ca0200dbdc84d4f2f0d68d45d382f
KEUMYUNSU <rmadbstn@naver.com>
2022-10-11 15:21:01 +0900

    git push origin master

---
+++ src/SamSung/BaekJoon15684.java
@@ -0,0 +1,34 @@
+package SamSung;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon15684 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		String[] strArr = br.readLine().split(" ");
+		
+		int N = Integer.parseInt(strArr[0]);  //세로선 개수 
+		int M = Integer.parseInt(strArr[0]);  //가로선 개수 
+		int H = Integer.parseInt(strArr[0]);  //세로선마다 가로선을 놓을 수 있는 위치의 개수.
+		
+		for(int i=0;i<M;i++) {
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+	}
+
+}
