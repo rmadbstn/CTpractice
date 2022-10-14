5c6fb1932f5a7dc435f3dbc9ac824f190367ac78
KEUMYUNSU <rmadbstn@naver.com>
2022-10-14 17:30:44 +0900

    BaekJoon15684 ing

--- src/PracticeNote.java
+++ src/PracticeNote.java
@@ -1,13 +1,31 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PracticeNote {
+	
+ public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException {
+	 
+	 	LinkedList<int[]> list = new LinkedList<int[]>();
+	 
+	 	list.add(new int[] {1,2,3});
+	 	
+	 	
+	 	Queue queue = new LinkedList<int[]>();
+	 	
+	 	for(int[] a:list) {
+	 		
+	 		
+	 		queue.offer(a);
+	 	}
+	 	
+	 	int[] arr = (int[])queue.poll();
+	 
+	 	
+	 	System.out.println(arr[0]);
+	 	
 
-
-		Scanner sc = new Scanner(System.in);
-		
 	}
 
 }
