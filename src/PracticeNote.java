d48559fb64a2070daec296c0dbd603257148ad60
KEUMYUNSU <rmadbstn@naver.com>
2022-10-24 19:58:49 +0900

    BaekJoon Study

--- src/PracticeNote.java
+++ src/PracticeNote.java
@@ -1,17 +1,16 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class PracticeNote {
 	
  public static void main(String[] args) throws IOException{
 
-	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	 String[] strArr = br.readLine().split(" ");
+	 Scanner sc = new Scanner(System.in);
+	 
+	 int N = sc.nextInt();
+	 for(int i=0;i<N;i++) {
+		 
+	 }
 	 
 
 	}
