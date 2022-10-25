1578e85f4ec707331a51060d38563a5ebe4847b3
KEUMYUNSU <rmadbstn@naver.com>
2022-10-25 15:29:32 +0900

    BaekJoon10926 finish

---
+++ src/Etc/BaekJoon10926.java
@@ -0,0 +1,20 @@
+package Etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon10926 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String Name = br.readLine();
+		
+		System.out.println(Name+"??!");
+		
+		
+	}
+
+}
